ac185480484399b9ff3facd0eb5d0ed164944107
tonmoy-free <tonmoykhan.free@gmail.com>
2025-03-12 22:23:56 +0600

    desktop part complete title dynamic update

--- script/index.js
+++ script/index.js
@@ -166,7 +166,7 @@ const displayVideos = (videos) => {
                           </div>
                     </div>
                     <div class="intro">
-                        <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+                        <h2 class="text-sm font-semibold">${video.title}</h2>
                         <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name} 
                         ${video.authors[0].verified == true ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="" srcset="">` : ``}
                         
